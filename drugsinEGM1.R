f7b6f0558141d0a34155192a8d5ca287b51eb7a3
cynthiagaoUOA <cynthia.gao@auckland.ac.nz>
2026-03-07 14:33:22 +1300

    Analysing data of first EGM drug run

---
+++ drugsinEGM1.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(vascr)
+
+drugsinEGM1 <- vascr_import("ECIS", "ECIS_260223_MFT_1_CG_drugscreenpaper_rep.abp", "ECIS_260223_MFT_1_CG_drugscreenpaper_rep_RbA.csv", "rep1")
+
